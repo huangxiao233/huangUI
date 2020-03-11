a0f3441ca06511b835ba13d6c7601e12a4bc736a
黄笑 <huangxiao221@jd.com>
2020-03-11 15:32:02 +0800

    配置externals,production模式下可以打包压缩得更小

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode:'development',      //production 会压缩代码的，影响最后web server的提示
+    mode:'production',      //production 会压缩代码的，影响最后web server的提示
     entry:{
         index:'./lib/index.tsx'
     },
@@ -27,5 +27,19 @@ module.exports = {
             template: 'index.html'
         })
     ],
-    
+    externals: {
+        react: {
+            //几种不同的打包方式。
+            commonjs: 'react',    //module.export 方式
+            commonjs2: 'react',   //也是一种node 的标准
+            amd: 'react',        //浏览器的标准
+            root: 'React', // 用script标签来引入React
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        },
+    }
 }
