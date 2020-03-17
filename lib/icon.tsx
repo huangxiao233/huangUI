6658d934f2629b3a2bb4d5d4b18f3f521b446933
黄笑 <huangxiao221@jd.com>
2020-03-17 11:39:06 +0800

    支持多个ICON

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './icons/wechat.svg'
+import './icons/qq.svg'
+import './icons/alipay.svg'
 
 
 interface IconProps{
@@ -8,11 +10,22 @@ interface IconProps{
 
 const Icon: React.FunctionComponent<IconProps> = (props)=>{
     return (
-           <svg>
-                {/* <use xlinkHref={`#${props.name}`}></use> */}
-                <use xlinkHref={'#wechat'}></use>
-                
-           </svg>
+        //    <svg>
+        //         {/* <use xlinkHref={`#${props.name}`}></use> */}
+        //         <use xlinkHref={'#wechat'}></use>       
+        //    </svg>
+          
+        <div>
+        <svg>
+            <use xlinkHref={'#alipay'}></use>        
+        </svg>
+        <svg>
+            <use xlinkHref={'#qq'}></use>    
+        </svg>
+         <svg>
+         <use xlinkHref={'#wechat'}></use>        
+        </svg>
+        </div>
 
     )
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,4 +3,4 @@ import ReactDom from 'react-dom'
 // import Button from './button'
 import Icon from './icon'
 
-ReactDom.render(<div><Icon name="wechat"/></div>,document.querySelector('#root'));
+ReactDom.render(<div><Icon name="alipay"/></div>,document.querySelector('#root'));
