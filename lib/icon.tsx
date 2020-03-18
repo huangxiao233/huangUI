c8a5b6fdeacea5908bd882f9e0ac7bdfaf5ed963
黄笑 <huangxiao221@jd.com>
2020-03-18 21:14:02 +0800

    支持点击

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './importAllIcon'
 import './icon.scss'
 interface IconProps {
-    name: string
+    name: string,
+    onClick:React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-            <svg className="huangUI-icon ">
+            <svg className="huangUI-icon " onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`}></use>
             </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,4 +3,7 @@ import ReactDom from 'react-dom'
 // import Button from './button'
 import Icon from './icon'
 
-ReactDom.render(<div><Icon name="alipay" /><Icon name="wechat" /><Icon name="qq" /></div>, document.querySelector('#root'));
+const fn:React.MouseEventHandler =(e)=>{
+    console.log(e)
+}
+ReactDom.render(<div><Icon name="alipay" onClick={fn}/></div>, document.querySelector('#root'));
